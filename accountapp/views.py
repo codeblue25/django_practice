9fc3071a8b1cbdd275407c44173f6a0d0f231683
codeblue25 <chois1031@gmail.com>
2021-07-19 22:22:53 +0900

    Save user ID on DB with POST method

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import User
 
 
 def hello_1ban(request):
 
     if request.method == 'POST':
-        return render(request, 'accountapp/hello_1ban.html', context={'text': 'POST METHOD!!'})
+
+        temp = request.POST.get('user_id')
+
+        new_user = User()
+        new_user.text = temp
+        new_user.save()
+
+        return render(request, 'accountapp/hello_1ban.html', context={'user_output': new_user})
     else:
         return render(request, 'accountapp/hello_1ban.html', context={'text': 'GET METHOD!!'})
