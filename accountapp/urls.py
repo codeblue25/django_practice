81f3eb7666ed9f932f3d129d1f32b4cb56b919a6
codeblue25 <chois1031@gmail.com>
2021-07-20 18:07:38 +0900

    Implement a CreateView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_1ban
+from accountapp.views import hello_1ban, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_1ban/', hello_1ban, name='hello_1ban')
+    path('hello_1ban/', hello_1ban, name='hello_1ban'),
+
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import User
 
@@ -22,3 +26,9 @@ def hello_1ban(request):
     else:
         user_list = User.objects.all()
         return render(request, 'accountapp/hello_1ban.html', context={'user_list': user_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_1ban')
+    template_name = 'accountapp/create.html'
